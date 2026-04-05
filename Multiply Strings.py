9bb663d092951bf2396ed0306934622a95f533e4
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-05 16:42:41 -0700

    Update Multiply Strings.py

--- Multiply Strings.py
+++ Multiply Strings.py
@@ -1,4 +1,5 @@
-# comments should explain everything
+# You have two strings which are the shape of positive numbers.
+# You have to multiply them without directly turning them into integers.
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         m, n = len(num1), len(num2)
@@ -6,11 +7,14 @@ class Solution:
         
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
-                mul = (ord(num1[i]) - 48) * (ord(num2[j]) - 48) # literally multiply digits
-                part = mul + pos[i + j + 1] # add to what was previously there
+                # literally multiply digits
+                mul = (ord(num1[i]) - 48) * (ord(num2[j]) - 48)
 
-                pos[i + j] += part // 10  # carry, you have to keep adding to it
-                pos[i + j + 1] = part % 10  # non-carry, it's the right value
+                # add to what was previously there
+                carry, rem = divmod(mul + pos[i + j + 1], 10)
+
+                pos[i + j] += carry  # carry, you have to keep adding to it
+                pos[i + j + 1] = rem  # non-carry, it's the right value
 
         # you turn the list into a string
         # then delete the trailing zeroes at the beginning
