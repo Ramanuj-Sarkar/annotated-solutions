7fcae7fe9c9a0fc104ed78140ff91ea95c7765e0
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-05 16:19:48 -0700

    Update Merge k Sorted Lists.py

--- Merge k Sorted Lists.py
+++ Merge k Sorted Lists.py
@@ -1,3 +1,6 @@
+# Merge k sorted linked lists.
+# Return None if there are no lists.
+# 
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,32 +8,31 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]: 
-        if not lists or len(lists) == 0:
+        if not lists:  # no lists
             return None
         
-        while len(lists) > 1:
-            temp = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i+1] if i + 1 < len(lists) else None
-                temp.append(self.merge_lists(l1, l2))
-            lists = temp
+        l1 = lists.pop()
         
-        return lists[0]
+        while lists:  # while there are more lists
+            l2 = lists.pop()  # take this list out
+            l1 = self.merge_lists(l1, l2)  # merge it with the old one
+        
+        return l1
     
     def merge_lists(self, l1, l2):
         node = ListNode()
-        ans = node
+        ans = node  # lets you return the head
         
         while l1 and l2:
-            if l1.val > l2.val:
+            if l1.val > l2.val:  # add the node in l2 if it's smaller
                 node.next = l2
                 l2 = l2.next
             else:
-                node.next = l1
+                node.next = l1  # add the node in l1 otherwise
                 l1 = l1.next
             node = node.next
-        
+
+        # add the remaining nodes to the new linked list
         if l1:
             node.next = l1
         else:
