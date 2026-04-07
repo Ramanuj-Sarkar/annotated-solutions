282bb39086bdd53f22a0a803820e4d190b26f54d
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-07 09:50:13 -0700

    Update Median of Two Sorted Arrays.py

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -16,25 +16,34 @@ class Solution:
             partX = (low + high) // 2
             partY = (x + y + 1) // 2 - partX
             
-            # this is the maximum left value and minimum right value of the smaller array
+            # this stars as the maximum value of the left half of the smaller array
+            # and the minimum value of the right half of that array
+            # and eventually becomes these values with regards to the whole thing
             maxlX = float('-inf') if partX == 0 else nums1[partX - 1]
             minrX = float('inf') if partX == x else nums1[partX]
             
-            # this is the maximum left value and minimum right value of the larger array
+            # this is the same thing for the larger array
             maxlY = float('-inf') if partY == 0 else nums2[partY - 1]
             minrY = float('inf') if partY == y else nums2[partY]
             
-            # there is an overlap here
+            print(maxlX, minrX, "vs", maxlY, minrY)
+
             if maxlX <= minrY and maxlY <= minrX:
+                # the left and right halves of both arrays correspond
+                # the values keep moving such that the median is
+                # eventually in the left arrays if it's odd
+
                 if (x + y) % 2 == 0:  # even type of median (between two values)
                     return (max(maxlX, maxlY) + min(minrX, minrY)) / 2.0
                 else:  # odd type of median (single value)
                     return float(max(maxlX, maxlY))
             elif maxlX > minrY:
                 # the median has to be to the left
+                print("left")
                 high = partX - 1
             else:
                 # the median has to be to the right
+                print("right")
                 low = partX + 1
                 
         return 0.0
