4747096b4b41518da644fd0a3215be8909e70d95
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2026-04-11 11:44:11 -0700

    Update Number of Islands.py
    I added this new version because I wrote it later and have a better understanding.

--- Number of Islands.py
+++ Number of Islands.py
@@ -1,36 +1,59 @@
+# We have a grid where '0' is water and '1' is land.
+# For each piece of land, cells connected horizontally and vertically are part of the same piece of land
+# while cells connected diagonally are not.
+# We have to find the number of connected series of 1.
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # the grid doesn't exist
-        if not grid:
-            return 0
-        
-        # find the number of islands
-        num_islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    # set all contiguous parts of the island to seen / water
-                    self.dfs(grid, i, j)
-                    num_islands += 1
+        answer = 0
 
-        return num_islands
+        # store all visited cells
+        visited = set()
 
-    def dfs(self, grid, r, c):
-        
-        if (r < 0
-            or c < 0
-            or r >= len(grid)
-            or c >= len(grid[0])
-            or grid[r][c] == "0"):
-            
-            return
-        
-        # set it to seen
-        grid[r][c] = "0"
+        # m = rows, n = columns
+        m, n = len(grid), len(grid[0])
 
-        # set the ones around it to seen
-        self.dfs(grid, r - 1, c)
-        self.dfs(grid, r + 1, c)
-        self.dfs(grid, r, c - 1)
-        self.dfs(grid, r, c + 1)
+        for x in range(m):
+            for y in range(n):
+                curr = (x, y)
+                if curr not in visited and grid[x][y] == '1':
+                    # add it to the islands
+                    answer += 1
+
+                    # queue for BFS
+                    positions = []
+                    positions.append(curr)
+
+                    while positions:
+                        this_x, this_y = positions[0]
+
+                        # add positions to visited
+                        visited.add(positions[0])
+
+                        # you're just checking the corresponding cell
+                        # and adding it if it's 1 and hasn't been visited yet
+                        if this_x > 0:
+                            up = (this_x - 1, this_y)
+                            if up not in visited and grid[up[0]][up[1]] == '1':
+                                visited.add(up)
+                                positions.append(up)
+                        if this_x < m - 1:
+                            down = (this_x + 1, this_y)
+                            if down not in visited and grid[down[0]][down[1]] == '1':
+                                visited.add(down)
+                                positions.append(down)                            
+                        if this_y > 0:
+                            left = (this_x, this_y - 1)
+                            if left not in visited and grid[left[0]][left[1]] == '1':
+                                visited.add(left)
+                                positions.append(left)                            
+                        if this_y < n - 1:
+                            right = (this_x, this_y + 1)
+                            if right not in visited and grid[right[0]][right[1]] == '1':
+                                visited.add(right)
+                                positions.append(right)
+
+                        # queue behavior
+                        positions = positions[1:]
+
+        return answer
         
